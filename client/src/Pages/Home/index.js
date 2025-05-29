e32f1603b2298160b3b218780e125f5db62d71bb
Kenil Shah <132459025+shahkenil01@users.noreply.github.com>
2025-05-29 15:57:47 +0530

    quick update

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import HomeBanner from "../../Components/HomeBanner";
 import ProductItem from "../../Components/ProductItem";
 import HomeCat from "../../Components/HomeCat";
 import images from '../../assets/images.js';
-import Button from '@mui/material/Button';
-import { IoIosArrowRoundForward } from "react-icons/io";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { fetchDataFromApi } from '../../utils/api';
+
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 const CustomNextArrow = ({ onClick, isDisabled }) => (
     <div className={`homeBanner-next-btn popular-products-next ${isDisabled ? "disabled" : ""}`} onClick={!isDisabled ? onClick : null} />
@@ -26,6 +28,10 @@ const handleSwiperUpdate = (swiper) => {
 
 const Home = () => {
     const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => setValue(newValue);
 
     useEffect(() => {
         fetchDataFromApi('/api/products').then(res => {
@@ -33,6 +39,14 @@ const Home = () => {
                 setProducts(res);
             }
         });
+    }, []);
+
+    useEffect(() => {
+        const order = ["Fashion", "Electronics", "Bags", "Footwear", "Groceries", "Beauty", "Wellness", "Jewellery"];
+        fetchDataFromApi('/api/category/all')
+            .then(res => res && setCategories(res.sort((a, b) =>
+                (order.indexOf(a.name.trim()) ?? 999) - (order.indexOf(b.name.trim()) ?? 999)
+            )));
     }, []);
 
     return (
@@ -58,9 +72,18 @@ const Home = () => {
                             <div className="info-1 d-flex align-item-center">
                                 <div className="info">
                                     <h3 className="mb-0 hd">Popular Products</h3>
-                                    <p className="text-light text-sml mb-0">Do not miss the current offers until the end of Feb.</p>
+                                    <p className="text-light text-sml mb-0">Do not miss the current offers until the end of May.</p>
                                 </div>
-                                <Button className="viewAllBtn ml-auto">View All<IoIosArrowRoundForward /></Button>
+                                <Box sx={{ maxWidth: '100%', bgcolor: 'background.paper', overflow: 'hidden' }} >
+                                    <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons="auto"
+                                    allowScrollButtonsMobile aria-label="scrollable category tabs" 
+                                    sx={{  minHeight: 48, "& .MuiTabs-scrollButtons": { color: "#000" }}}>
+                                        {categories.map((cat, index) => (
+                                            <Tab key={cat._id} label={cat.name} className="item" 
+                                            sx={{ minWidth: 95,  maxWidth: 95, flexShrink: 0, textTransform: "capitalize", paddingX: 1 }}/>
+                                        ))}
+                                    </Tabs>
+                                </Box>
                             </div>
 
                             <div className="product_row w-100 mt-2">
@@ -91,7 +114,6 @@ const Home = () => {
                                     <h3 className="mb-0 hd">NEW PRODUCTS</h3>
                                     <p className="text-light text-sml mb-0">New products with updated stocks.</p>
                                 </div>
-                                <Button className="viewAllBtn ml-auto">View All<IoIosArrowRoundForward /></Button>
                             </div>
 
                             <div className="product_row productRow2 w-100 mt-3 d-flex flex-wrap">

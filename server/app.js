6f770f20c4c76f00bcb4403aa5b22a4f30c1b5e9
Kenil Shah <132459025+shahkenil01@users.noreply.github.com>
2025-06-01 22:19:36 +0530

    quick update

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 require('dotenv/config');
 
 app.use(cors({

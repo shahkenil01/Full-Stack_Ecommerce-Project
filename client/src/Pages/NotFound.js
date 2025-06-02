63508a740b002e2755c35e8ff3e9e99474638b2c
Kenil Shah <132459025+shahkenil01@users.noreply.github.com>
2025-06-02 16:29:52 +0530

    quick update

--- client/src/Pages/NotFound.js
+++ client/src/Pages/NotFound.js
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from 'react';
-import { MyContext } from '../../App';
+import { MyContext } from '../App';
 import { Link } from 'react-router-dom';
 
 function NotFound() {

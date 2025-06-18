d656138e79b797860d2647be6d86bab5a4e1afbf
Kenil Shah <132459025+shahkenil01@users.noreply.github.com>
2025-06-18 18:39:26 +0530

    quick update

--- client/src/Pages/ProductDetails/index.js
+++ client/src/Pages/ProductDetails/index.js
@@ -233,81 +233,55 @@ const ProductDetails = () =>{
                                         <tbody>
                                         <tr className="stand-up">
                                           <th>Stand Up</th>
-                                          <td>
-                                            <p>35”L x 24”W x 37-45”H (front to back wheel)</p>
-                                          </td>
+                                          <td> <p>35”L x 24”W x 37-45”H (front to back wheel)</p> </td>
                                         </tr>
                                         <tr className="folded-wo-wheels">
                                           <th>Folded (w/o wheels)</th>
-                                          <td>
-                                            <p>32.5"L x 18.5”W x 16.5”H</p>
-                                          </td>
+                                          <td> <p>32.5"L x 18.5”W x 16.5”H</p> </td>
                                         </tr>
                                         <tr className="folded-w-wheels">
                                           <th>Folded (w/ wheels)</th>
-                                          <td>
-                                            <p>32.5"L x 24”W x 18.5”H</p>
-                                          </td>
+                                          <td> <p>32.5"L x 24”W x 18.5”H</p> </td>
                                         </tr>
                                         <tr className="door-pass-through">
                                           <th>Door Pass Through</th>
-                                          <td>
-                                            <p>24"</p>
-                                          </td>
+                                          <td> <p>24"</p> </td>
                                         </tr>
                                         <tr className="weight-wo-wheels">
                                           <th>Weight (w/o wheels)</th>
-                                          <td>
-                                            <p>20 LBS</p>
-                                          </td>
+                                          <td> <p>20 LBS</p> </td>
                                         </tr>
                                         <tr className="weight-capacity">
                                           <th>Weight Capacity</th>
-                                          <td>
-                                            <p>60 LBS</p>
-                                          </td>
+                                          <td> <p>60 LBS</p> </td>
                                         </tr>
                                         <tr className="width">
                                           <th>Width</th>
-                                          <td>
-                                            <p>24"</p>
-                                          </td>
+                                          <td> <p>24"</p> </td>
                                         </tr>
                                         <tr className="handle-height-ground-to-handle">
                                           <th>Handle height (ground to handle)</th>
-                                          <td>
-                                            <p>37-45"</p>
-                                          </td>
+                                          <td> <p>37-45"</p> </td>
                                         </tr>
                                         <tr className="wheels">
                                           <th>Wheels</th>
-                                          <td>
-                                            <p>12” air / wide track slick tread</p>
-                                          </td>
+                                          <td> <p>12” air / wide track slick tread</p> </td>
                                         </tr>
                                         <tr className="seat-back-height">
                                           <th>Seat back height</th>
-                                          <td>
-                                            <p>21.5"</p>
-                                          </td>
+                                          <td> <p>21.5"</p> </td>
                                         </tr>
                                         <tr className="head-room">
                                           <th>Head room (inside canopy)</th>
-                                          <td>
-                                            <p>25"</p>
-                                          </td>
+                                          <td> <p>25"</p> </td>
                                         </tr>
                                         <tr className="pa_color">
                                           <th>Color</th>
-                                          <td>
-                                            <p>Black, Blue, Red, White</p>
-                                          </td>
+                                          <td> <p>Black, Blue, Red, White</p> </td>
                                         </tr>
                                         <tr className="pa_size">
                                           <th>Size</th>
-                                          <td>
-                                            <p>M, S</p>
-                                          </td>
+                                          <td> <p>M, S</p> </td>
                                         </tr>
                                     </tbody>
                                 </table>

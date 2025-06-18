94300204fa4c42bea5d912ab839be2d697aef630
Kenil Shah <132459025+shahkenil01@users.noreply.github.com>
2025-06-18 22:42:29 +0530

    quick update

--- client/src/Components/MuiToast/index.js
+++ client/src/Components/MuiToast/index.js
@@ -1,7 +1,7 @@
-import react from 'react';
 import { SnackbarProvider, useSnackbar } from 'notistack';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import Fade from '@mui/material/Fade';
 
 const SnackbarCloseButton = ({ snackbarKey }) => {
   const { closeSnackbar } = useSnackbar();
@@ -14,8 +14,8 @@ const SnackbarCloseButton = ({ snackbarKey }) => {
 };
 
 const NotistackProvider = ({ children }) => (
-  <SnackbarProvider maxSnack={6} autoHideDuration={3000} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-    action={(key) => <SnackbarCloseButton snackbarKey={key} />} >
+  <SnackbarProvider maxSnack={6} autoHideDuration={3000} anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} 
+    TransitionComponent={Fade} action={(key) => <SnackbarCloseButton snackbarKey={key} />} >
     {children}
   </SnackbarProvider>
 );

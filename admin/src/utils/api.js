b4426d497b145fc9d971748f9363ecc1975a878d
shahkenil01 <shahkenil430@gmail.com>
2026-04-20 14:55:11 +0530

    quick update

--- admin/src/utils/api.js
+++ admin/src/utils/api.js
@@ -17,6 +17,12 @@ const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("userToken");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+}, (error) => Promise.reject(error));
+
 axiosInstance.interceptors.response.use(
   (res) => res,
   (err) => {
